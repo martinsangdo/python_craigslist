4a23cdbbb5da28a3a030e9aebc1e296594b92cf7
Martin SangDo <sang.do@knorex.com>
2022-03-26 16:19:03 +0700

    - scrape list

--- scraping/scraping.py
+++ scraping/scraping.py
@@ -4,11 +4,11 @@
 from lxml import html
 
 import requests
-import time
 from pymongo import MongoClient
 import constant
 import calendar
-
+import datetime
+import time
 #######################
 def getCurrentTimestamp():
     return calendar.timegm(time.gmtime())
@@ -57,13 +57,13 @@ def parse_post_list_page(db_client, meta_detail, post_list_page_url):
             continue
     # print page.content
     tree = html.fromstring(page.content)
-    print 'completed parse ' + post_list_page_url
+    print('completed parse ' + post_list_page_url)
     list = tree.xpath('//ul[@id="search-results"]/li[@class="result-row"]')
     for item in list:
         detail = {}
         #find date
         date = item.xpath('.//time[@class="result-date"]')
-        detail['datetime'] = date[0].attrib['datetime']
+        detail['datetime'] = convert_2_timestamp(date[0].attrib['datetime'], '%Y-%m-%d %H:%S')
         #find link
         link = item.xpath('.//a')
         parse_detail_page(db_client, meta_detail, detail, link[0].attrib['href'])
@@ -148,7 +148,11 @@ def parse_page(db_client):
                 parse_city_list_page(db_client, meta_detail, country.attrib['href'])
             else:
                 parse_city_page(db_client, meta_detail, country.attrib['href'])
-
+#######################
+def convert_2_timestamp(str_date, dateformat):
+    date = datetime.datetime.strptime(str_date, dateformat)
+    timestamp = datetime.datetime.timestamp(date)
+    return round(timestamp)
 #######################
 start_time = getCurrentTimestamp()
 client = MongoClient('localhost:27017')
@@ -159,8 +163,9 @@ db_client = client['craigslist_db']
 #test
 meta_detail = {}
 meta_detail['country'] = 'americas'
-# parse_city_page(db_client, meta_detail, 'https://losangeles.craigslist.org')
-parse_city_list_page(db_client, meta_detail, 'https://www.craigslist.org/about/sites#CA')
+parse_city_page(db_client, meta_detail, 'https://losangeles.craigslist.org')
+# parse_city_list_page(db_client, meta_detail, 'https://www.craigslist.org/about/sites#CA')
+# print(convert_2_timestamp("2022-03-25 22:40", '%Y-%m-%d %H:%S'))
 
 #
 end_time = getCurrentTimestamp()
